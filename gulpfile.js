a47e605866a8f739453a523be0646cb32e9e18fa
DESKTOP-9LU36LJ\Owner <aoneko733@gmail.com>
2018-11-08 15:40:11 +0900

    commit 2018-11-08-15-40

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+browserSync = require('browser-sync'),
+autoprefixer = require('gulp-autoprefixer'),
+sass = require('gulp-sass');
 
 gulp.task("sass", function() {
   return gulp.src('./src/scss/*.scss')
     .pipe(sass({
       outputStyle:'expanded'
     }))
-    .pipe(gulp.dest('./wordpress/wp-content/themes/wp-test-2018/'));
+    .pipe(autoprefixer({
+      browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./wordpress/wp-content/themes/aonekocafe/'));
 });
 
 gulp.task("default", ['sass']);
+gulp.task('watch', function(){
+  gulp.watch('./src/scss/*.scss', ['sass']);
+});
